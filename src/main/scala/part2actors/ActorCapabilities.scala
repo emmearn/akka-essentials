3d54979de862f2d5e6cc173e8240c69b75074ba7
Marco Arnone <marcoarnone@easy-network.it>
2019-01-31 10:15:50 +0100

    actors exercises

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -1,6 +1,7 @@
 package part2actors
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import part2actors.ActorCapabilities.Person.LiveTheLife
 
 object ActorCapabilities extends App {
 
@@ -53,4 +54,97 @@ object ActorCapabilities extends App {
 
   case class WirelessPhoneMessage(content: String, ref: ActorRef)
   alice ! WirelessPhoneMessage("Hi", bob) // no sender
+
+  class Counter extends Actor {
+    import Counter._
+    var count = 0
+
+    override def receive: Receive = {
+      case Increment => count += 1
+      case Decrement => count += 1
+      case Print => println(s"[counter] My current count is ${count}")
+      case _ => println("unknown command")
+    }
+  }
+
+
+
+  object Counter {
+    // DOMAIN of the counter
+    case object Increment
+    case object Decrement
+    case object Print
+
+    def props = Props(new Counter)
+  }
+
+  val counter = system.actorOf(Counter.props, "myCounter")
+
+  import Counter._
+
+  counter ! Print
+  (1 to 3).foreach(_ => counter ! Increment)
+  counter ! Print
+  counter ! Decrement
+  counter ! Print
+
+
+
+  object BankAccount {
+    case class Deposit(amount: Int)
+    case class Withdraw(amount: Int)
+    case object Statement
+
+    case class TransactionSuccess(message: String)
+    case class TransactionFailure(reason: String)
+
+    def props = Props(new BankAccount)
+  }
+
+  class BankAccount extends Actor {
+    import BankAccount._
+    var founds = 0
+
+    override def receive: Receive = {
+      case Deposit(amount) =>
+          if(amount < 0)
+            sender ! TransactionFailure(s"Invalid deposit ${amount}")
+          else {
+            founds += amount
+            sender ! TransactionSuccess(s"Successfully deposit ${amount}")
+          }
+      case Withdraw(amount) =>
+        if(amount < 0)
+          sender ! TransactionFailure(s"Invalid withdraw ${amount}")
+        else if(amount > founds)
+          sender ! TransactionFailure("Insufficient founds")
+        else {
+          founds -= amount
+          sender ! TransactionSuccess(s"Successfully withdrew ${amount}")
+        }
+      case Statement => sender ! s"You balance is $founds"
+    }
+  }
+
+  object Person {
+    case class LiveTheLife(account: ActorRef)
+  }
+  class Person extends Actor {
+    import Person._
+    import BankAccount._
+
+    override def receive: Receive = {
+      case LiveTheLife(account) =>
+        account ! Deposit(10000)
+        account ! Withdraw(90000)
+        account ! Withdraw(500)
+        account ! Statement
+      case message => println(message.toString)
+    }
+  }
+
+  val account = system.actorOf(BankAccount.props, "bankAccount")
+  val person = system.actorOf(Props[Person], "bilionaire")
+
+  person ! LiveTheLife(account)
 }
